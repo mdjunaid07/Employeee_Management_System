372e3a3353c998122a2934c74305080dca5c92bf
junaid <itsaboycse@gmail.com>
2025-02-03 22:04:37 +0530

    fix: add base path and nojekyll for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
+  
+  base: "Employee_Management_System", // Replace with your actual GitHub repo name
   plugins: [react()],
 })
